502c1e5816929a16984abfcc68bd48c9c1e886ce
miguelspm <142408412+miguelspm@users.noreply.github.com>
2023-08-23 14:00:31 +0000

    enviar git

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,16 @@
+/*******************
+  Autor: Miguel M.
+  Data: 23/03/2023
+
+  Programa basico em C
+********************/
+#include <stdio.h>
+#include <math.h>
+
+//funcao principal
+
+int main() {
+ printf("ola pessoal;\n");
+printf("raiz quadrada de 2 é %f\n", sqrt(2));
+  return 0;
+}
---
+++ praticas/pratica01/questao01.c
@@ -0,0 +1,11 @@
+#include <sdtio.h>
+
+int main() {
+float nota_a1 - 5.5f;
+float nota_a2 - 6.0f;
+
+ float media -  0.4 * nota_a1 + 0.6 * nota_a2;
+
+  printf("a média é %f\n", media);
+  return 0;
+}
