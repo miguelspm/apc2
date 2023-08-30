cd9736f66685d81ce216af7e2a51dc46ecc4ef7b
miguelspm <142408412+miguelspm@users.noreply.github.com>
2023-08-30 13:19:10 +0000

    aula02

---
+++ aulas/variaveis.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+  char caracter; // 'a', 'b', 'c', -128 a 127 - 1 byte
+
+unsigned char caracter_sem_sinal; // 0 a 255
+  
+  char i, j;
+  
+  char string[10] // "um texto"
+  
+  int inteiro; // -2147483648 a 2147483647 - 4 byte
+
+  unsigned int (inteiro_sem_sinal); // 0 a 4294967295
+
+  short int inteiro_curto; // -32768 a 32767 - 2 bytes
+
+  
+  
+  float flutuante; //0.123456f 4 bytes
+  
+  double duoplo; // 0.12345678912345 - bytes
+  
+  void nada; // 1 byte
+
+   return 0;
+}
---
+++ praticas/pratica01/questao02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ int base = 12;
+  int altura = 6;
+
+  int area = base * altura/2;
+
+  printf("o valor da area é %i\n", area);
+
+}
+
+  
